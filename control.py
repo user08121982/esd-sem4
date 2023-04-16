b86eeeab848bda2f065a2f8ef239bba63a301207
user08121982 <avinashrsingh1705@gmail.com>
2023-04-16 10:35:27 +0530

    class for selector

--- control.py
+++ control.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
-import socket
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 app=Flask(__name__)
 
 d1,d2,d3=2,3,4
 s1,s2,s3=False,False,False
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(d1,GPIO.OUT)
-# GPIO.setup(d2,GPIO.OUT)
-# GPIO.setup(d3,GPIO.OUT)
-# GPIO.output(d1,0)
-# GPIO.output(d2,0)
-# GPIO.output(d3,0)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(d1,GPIO.OUT)
+GPIO.setup(d2,GPIO.OUT)
+GPIO.setup(d3,GPIO.OUT)
+GPIO.output(d1,0)
+GPIO.output(d2,0)
+GPIO.output(d3,0)
 print("Done")
 
 @app.route("/")
@@ -26,7 +25,7 @@ def d1Tog():
     global s1
     if request.method=='POST':
         s1=not s1
-        # GPIO.output(d1,s1)
+        GPIO.output(d1,s1)
     return str(s1)
 
 @app.route("/b2",methods=['GET','POST'])
@@ -34,7 +33,7 @@ def d2Tog():
     global s2
     if request.method=='POST':
         s2=not s2
-        # GPIO.output(d2,s2)
+        GPIO.output(d2,s2)
     return str(s2)
 
 @app.route("/b3",methods=['GET','POST'])
@@ -42,9 +41,9 @@ def d3Tog():
     global s3
     if request.method=='POST':
         s3=not s3
-        # GPIO.output(d3,s3)
+        GPIO.output(d3,s3)
     return str(s3)
 
 if __name__=="__main__":
     print("Start")
-    app.run(host=socket.gethostbyname(socket.gethostname()),port=5010)
+    app.run(host='192.168.43.189',port=5010)
